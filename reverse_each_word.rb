e7087d3ca8478e12c390b31336390adde92a5697
Qun Li Huang <qunli02@gmail.com>
2019-04-01 19:58:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new = []
-  array.each do |word|
-    new.push(word.reverse)
-  end
+  new = array.collet { |word| word.reverse }
   return new.join(" ")
 end
